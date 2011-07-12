7453fd2f527ca8ec57201f9ce94347d97ed40d7d
Sascha Konietzke <sascha@thriventures.com>
2011-07-12 15:38:21 +0200

    Initial Commit

---
+++ StorageRoomRestKit/StorageRoomRestKitAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  StorageRoomRestKitAppDelegate.h
+//  StorageRoomRestKit
+//
+//  Created by Sascha Konietzke on 7/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class StorageRoomRestKitViewController;
+
+@interface StorageRoomRestKitAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet StorageRoomRestKitViewController *viewController;
+
+@end
---
+++ StorageRoomRestKit/StorageRoomRestKitViewController.h
@@ -0,0 +1,15 @@
+//
+//  StorageRoomRestKitViewController.h
+//  StorageRoomRestKit
+//
+//  Created by Sascha Konietzke on 7/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StorageRoomRestKitViewController : UIViewController {
+    
+}
+
+@end
